8457a314f187b5f32e880496caa998fd0f8db9f3
Franco131313 <franco.rojas.n@usach.cl>
2026-02-27 13:34:20 -0300

    Initial commit - ToolRent3 backend

---
+++ toolrent-backend/src/main/java/cl/franco/rojas/toolrent/toolrent_backend/ToolrentBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.franco.rojas.toolrent.toolrent_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolrentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolrentBackendApplication.class, args);
+	}
+
+}
---
+++ toolrent-backend/src/test/java/cl/franco/rojas/toolrent/toolrent_backend/ToolrentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.franco.rojas.toolrent.toolrent_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolrentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
